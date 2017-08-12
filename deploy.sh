62b749b60ef9011d7e82d7d0714396cf94cc7546
Chris Griffing <cmgriffing@gmail.com>
2017-08-12 01:56:10 -0700

    Initial deploy script and index.html fixes

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+git checkout -b gh-pages
+npm run i18n-build
+git add locale -f
+git commit -m "Deploying i18n builds"
+git push origin gh-pages -f
+git checkout master
+git branch -D gh-pages
